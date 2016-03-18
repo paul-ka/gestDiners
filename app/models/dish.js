f69513bd124bcb92cfaab8a0e710f37e088e7831
Jerome Bar <barjerome@hotmail.com>
2016-03-18 11:23:19 +0100

    formatting css guest and dish

--- app/models/dish.js
+++ app/models/dish.js
@@ -3,43 +3,44 @@ var mongoose = require('mongoose');
 
 
 var dishSchema = new mongoose.Schema({
-  meal: String,
-  ingredients: String
+	meal: String,
+	ingredients: String,
+	image: String
 });
 
 var Dish = {
-    
-    model: mongoose.model('Dish', dishSchema),
-    
-    create: function(req, res) {
+	model: mongoose.model('Dish', dishSchema),
+	create: function (req, res) {
 		Dish.model.create({
 			meal: req.body.meal,
-			ingredients: req.body.ingredients
-		}, function(){
+			ingredients: req.body.ingredients,
+			image: req.body.image
+		}, function () {
 			res.sendStatus(200);
 		})
 	},
 
-	findAll: function(req, res) {
+	findAll: function (req, res) {
 		Dish.model.find(function (err, data) {
 			res.send(data);
 		});
 	},
 
-	update: function(req, res){
+	update: function (req, res) {
 		Dish.model.findByIdAndUpdate(req.params.id, {
 			meal: req.body.meal,
-			ingredients: req.body.ingredients
-		}, function(){
+			ingredients: req.body.ingredients,
+			image: req.body.image
+		}, function () {
 			res.sendStatus(200);
 		})
 	},
 
-	delete: function(req, res){
-		Dish.model.findByIdAndRemove(req.params.id, function(){
+	delete: function (req, res) {
+		Dish.model.findByIdAndRemove(req.params.id, function () {
 			res.sendStatus(200);
 		})
-	} 
+	}
 }
 
 module.exports = Dish;
--- public/js/controllers/dishController.js
+++ public/js/controllers/dishController.js
@@ -1,31 +1,51 @@
 // MAIN CONTROLLER DISH
 function dishController($scope, $http, dishService) {
 	$scope.title = "Liste de plats";
-	
-	function load(){
-		dishService.get().then(function(res){
+
+	function load() {
+		dishService.get().then(function (res) {
 			$scope.dishs = res.data;
 		});
 	}
-	$scope.add = function(){
+	$scope.add = function () {
 		var data = {};
 		data.meal = $scope.meal;
 		data.ingredients = $scope.ingredients;
-		dishService.create(data).then(function(res){
+		data.image = $scope.imageFile;
+		dishService.create(data).then(function (res) {
 			load();
 		});
 		$scope.meal = "";
 		$scope.ingredients = "";
+		$scope.imageFile = "";
+		location.reload();
 	}
-	$scope.update = function(dish){
-		dishService.update(dish._id, dish).then(function(res){
+	$scope.update = function (dish) {
+		dishService.update(dish._id, dish).then(function (res) {
 			load();
 		});
 	}
-	$scope.delete = function(dish){
-		dishService.delete(dish._id).then(function(res){
+
+	$scope.delete = function (dish) {
+		dishService.delete(dish._id).then(function (res) {
 			load();
 		});
 	}
+
+	$scope.previewFile = function () {
+		var preview = document.querySelector('#preview');
+		var file = document.querySelector('input[type=file]').files[0];
+		var reader = new FileReader();
+		reader.onloadend = function () {
+			preview.src = reader.result;
+			$scope.imageFile = reader.result;
+		}
+		if (file) {
+			reader.readAsDataURL(file);
+		} else {
+			preview.src = "";
+		}
+	}
+
 	load();
 }
