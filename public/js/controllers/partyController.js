24d19ea84c67e5e4e8df9c68ee40a37e74617f4f
Jerome Bar <barjerome@hotmail.com>
2016-03-23 14:56:56 +0100

    move guest in an array

--- public/js/controllers/partyController.js
+++ public/js/controllers/partyController.js
@@ -1,25 +1,25 @@
 function partyController($scope, $http, partyService, dishService, guestService) {
 	$scope.title = "Soirée"
 
-	function load(){
-		partyService.get().then(function(res){
+	function load() {
+		partyService.get().then(function (res) {
 			$scope.partys = res.data;
 		});
-		dishService.get().then(function(res){
+		dishService.get().then(function (res) {
 			$scope.dishs = res.data;
 		});
-		guestService.get().then(function(res){
+		guestService.get().then(function (res) {
 			$scope.guests = res.data;
 		});
 
 	}
 	$scope.clickselect = function (guest) {
 		$scope.trusty = [];
-			
-		if ((guest.select == true) && (($scope.dishs.ingredients == $scope.guests.allergy) || ($scope.dishs.ingredients == $scope.guests.hated))){
+
+		/*if ((guest.select == true) && (($scope.dishs.ingredients == $scope.guests.allergy) || ($scope.dishs.ingredients == $scope.guests.hated))) {*/
+		if ((guest.select == true) && (($scope.partys.plats == $scope.guests.ingredients))) {
 			$scope.trusty = true;
-		}
-		else {
+		} else {
 			$scope.trusty = false;
 		};
 	}
